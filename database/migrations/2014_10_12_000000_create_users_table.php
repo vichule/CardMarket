a703e1d41cd74a1e9f53ae6e0e28eb6a63ad8197
javier <francisco_javier_cabanas_tsapp1ta2021@cev.com>
2022-02-03 16:15:02 +0100

    correcciones para p4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('Username')->unique();
+            $table->string('Email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('Password');
+            $table->enum('Particular','Profesional','Administrador');
             $table->rememberToken();
             $table->timestamps();
         });
